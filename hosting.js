86d14824e9ff6f3f7280543b44b3e539c85eea30
joseph6369828419 <joseph566184@gmail.com>
2024-08-12 23:26:18 +0530

    ok

--- hosting.js
+++ hosting.js
@@ -1,13 +1,14 @@
 // Import the Express module
 const express = require('express');
-
+const path = require('path');
 // Create an Express application
 const app = express();
 
 // Define a route for the root URL ('/')
 app.get('/', (req, res) => {
   // Send a 'Hello, World!' response
-  res.send('Hello, World!');
+  // Send an HTML file as the response
+  res.sendFile(path.join(__dirname+'/hosting1.html'));
 });
 
 // Start the server on port 3000
